e32dd423755bdfb99467ff8ed71791b5a339e555
Jonathan <jonathanmabilangan16@gmail.com>
2023-06-12 20:55:40 +0800

    Able to display lotto results through print

--- README.txt
+++ README.txt
@@ -1,2 +1,4 @@
 # Disclaimer: This application does not predict what will be the winning number combination of the future lotto results.
 
+# To run the spider type in command line
+  ``` scrapy crawl <spider name> ```
--- lotto_results/spiders/__init__.py
+++ lotto_results/spiders/__init__.py
@@ -2,23 +2,23 @@
 #
 # Please refer to the documentation for information on how to create and manage
 # your spiders.
+import scrapy
 
 from pathlib import Path
-
-import scrapy
+import w3lib.html
 
 
 class LottoSpider(scrapy.Spider):
     name = "lottos"
     allowed_domains = ["https://www.lottopcso.com/"]
 
-    def start_requests(self, user_input: list[dict]):
+    def start_requests(self):
         # Test run for start_request method alone
-        # user_input = [
-        #     {"month": "April", "day": "24", "year": "2017"},
-        #     {"month": "August", "day": "8", "year": "2016"},
-        #     {"month": "July", "day": "14", "year": "2021"},
-        # ]
+        user_input = [
+            {"month": "April", "day": "24", "year": "2022"},
+            {"month": "January", "day": "8", "year": "2023"},
+            {"month": "July", "day": "14", "year": "2021"},
+        ]
         urls = [
             f"https://www.lottopcso.com/pcso-lotto-result-{dates['month']}-{dates['day']}-{dates['year']}/"
             for dates in user_input
@@ -28,7 +28,18 @@ class LottoSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f"logs/{page}.html"
-        Path(filename).write_bytes(response.body)
-        self.log(f"Saved file logs/{filename}")
+        # page = response.url.split("/")[-2]
+        # filename = f"logs/{page}.html"
+        # Path(filename).write_bytes(response.body)
+        # self.log(f"Saved file logs/{filename}")
+        # for row in response.xpath('//*[@id="post-******"]/div[2]/figure[1]'):
+        for row in response.xpath('//*[contains(@class, "has-fixed-layout")]'):
+            # filename = f"logs/{row}.html"
+            # Path(filename).write_bytes(response.body)
+            # self.log(f"{row}")
+            # print(f"someone {row.css('td::text').get()}")
+            # print(f"headers {row.css('th::text').get()}")
+            draw = w3lib.html.remove_tags(row.xpath(".//strong").get())
+            result = w3lib.html.remove_tags(row.xpath(".//td")[1].get())
+
+            print(f"Lotto:{draw}, Result: {result}")
